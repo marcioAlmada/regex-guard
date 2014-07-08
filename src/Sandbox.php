81b61aaf504ed5db684f0bc199b109482d67884d
Márcio Almada <marcio3w@gmail.com>
2014-07-08 10:14:31 -0300

    add implementation for deault sandbox pass tests for regex runtime sandbox

---
+++ src/Sandbox.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace RegexGuard;
+
+use RegexGuard\Interfaces\SandboxInterface;
+use RegexGuard\Interfaces\ErrorHandlerInterface;
+
+class Sandbox implements SandboxInterface
+{
+    protected $errorHandler;
+
+    public function __construct(ErrorHandlerInterface $errorHandler)
+    {
+        $this->errorHandler = $errorHandler;
+    }
+
+    public function run(callable $closure, array $args = [], $throwOnError = true, $severity = E_WARNING)
+    {
+        $this->errorHandler->enable($throwOnError, $severity);
+        $result = call_user_func_array($closure, $args);
+        $this->errorHandler->disable();
+        return $result;
+    }
+}

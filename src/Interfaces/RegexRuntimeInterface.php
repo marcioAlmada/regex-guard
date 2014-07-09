1091956749a54c8efd17a9755e93c35aca2e981f
Márcio Almada <marcio3w@gmail.com>
2014-07-09 11:19:29 -0300

    add RegexRuntimeInterface

---
+++ src/Interfaces/RegexRuntimeInterface.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace RegexGuard\Interfaces;
+
+interface RegexRuntimeInterface
+{
+    public function __construct(SandboxInterface $handler);
+
+    /**
+     * Validates a given perl compatible regular expression
+     * 
+     * @param  string  $pattern pcre string
+     * @return \RegexGuard\Interfaces\RegexHelperInterface
+     */
+    public function throwOnError($option = true);
+
+    /**
+     * Validates a given perl compatible regular expression
+     * 
+     * @param  string  $pattern pcre string
+     * @return boolean          true when pcre string is valid, false otherwise
+     */
+    public function isRegexValid($pattern);
+
+    /**
+     * Same as \preg_filter but throws exception when an invalid pcre string is given
+     * 
+     * @link http://php.net/manual/en/function.preg-filter.php
+     */
+    public function filter($pattern, $subject, $limit = -1, $flags = 0);
+    /**
+     * Same as \preg_grep but throws exception when an invalid pcre string is given
+     * 
+     * @link http://php.net/manual/en/function.preg-grep.php
+     */
+    public function grep($pattern, $input, $flags = 0);
+    
+    /**
+     * Same as \preg_match but throws exception when an invalid pcre string is given
+     * 
+     * @link http://php.net/manual/en/function.preg-match.php
+     */
+    public function match($pattern, $subject, &$matches = null, $flags = 0, $offset = 0);
+    /**
+     * Same as \preg_match but throws exception when an invalid pcre string is given
+     * 
+     * @link http://php.net/manual/en/function.preg-match-all.php
+     */
+    public function matchAll($pattern, $subject, &$matches = null, $flags = PREG_PATTERN_ORDER, $offset = 0);
+    
+    /**
+     * Same as \preg_replace but throws exception when an invalid pcre string is given
+     * 
+     * @link http://php.net/manual/en/function.preg-replace.php
+     */
+    public function replace($pattern, $replacement , $subject, $limit = -1, &$count = null);
+    
+    /**
+     * Same as \preg_split but throws exception when an invalid pcre string is given
+     * 
+     * @link http://php.net/manual/en/function.preg-split.php
+     */
+    public function split($pattern, $subject, $limit = -1, $flags = 0);
+
+}

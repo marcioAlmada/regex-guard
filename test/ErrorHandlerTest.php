e721b3e6528ff09f68f3f182ffe2b459f2f44e17
Márcio Almada <marcio3w@gmail.com>
2014-08-06 02:49:18 -0300

    add more edge cases tests for default ErrorHandler

--- test/ErrorHandlerTest.php
+++ test/ErrorHandlerTest.php
@@ -8,7 +8,7 @@ namespace RegexGuard;
  */
 class ErrorHandlerTest extends \PHPUnit_Framework_TestCase
 {
-    public function test()
+    public function testStack()
     {
         $handler = new ErrorHandler;
         $stack = $handler->enable();
@@ -16,5 +16,39 @@ class ErrorHandlerTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($handler->disable());
         $this->assertFalse($handler->disable());
         $this->assertSame($stack, $handler->enable());
+    }
+
+    /**
+     * @expectedException PHPUnit_Framework_Error_Warning
+     * @expectedExceptionMessage signal b
+     */
+    public function testAutoDefuse()
+    {
+        $handler = new ErrorHandler;
+        $stack = $handler->enable(true, E_USER_WARNING);
+
+        try {
+            trigger_error('signal a', E_USER_WARNING);
+        } catch (RegexException $e){
+            $this->assertEquals('signal a', $e->getMessage());
+            trigger_error('signal b', E_USER_WARNING);
+        }
+    }
+
+    /**
+     * @expectedException PHPUnit_Framework_Error_Warning
+     * @expectedExceptionMessage signal c
+     */
+    public function testWithThowOnWarningOff()
+    {
+        $handler = new ErrorHandler;
+        $stack = $handler->enable(false, E_USER_WARNING);
+
+        trigger_error('signal a', E_USER_WARNING);
+        trigger_error('signal b', E_USER_WARNING);
+
+        $handler->disable();
+
+        trigger_error('signal c', E_USER_WARNING);
     }
 }
